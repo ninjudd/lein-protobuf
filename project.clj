3ce5fc5ad4e54f43cc8cd23de86956e12078e32e
Justin Balthrop <git@justinbalthrop.com>
2012-07-17 11:24:53 -0700

    change description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-protobuf "0.2.0-beta1"
-  :description "Leiningen plugin for clojure-protobuf."
+  :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/flatland/protobuf"
