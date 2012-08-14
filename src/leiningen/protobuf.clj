c1d4f2c74eb414fe62f2a3a3b43ac59b3057febf
Anthony Grimes <i@raynes.me>
2012-08-14 18:07:55 -0500

    Revert "Prevent java compilation caused by eval-in-project."
    This reverts commit d2ab500749e5c4789dea7f6b452a39e7cb52156f.

--- src/leiningen/protobuf.clj
+++ src/leiningen/protobuf.clj
@@ -31,7 +31,6 @@
   (io/file (get project :proto-path "resources/proto")))
 
 (def ^{:dynamic true} *compile-protobuf?* true)
-(def ^{:dynamic true} *compile-java?* true)
 
 (defn target [project]
   (doto (io/file (:target-path project))
@@ -117,8 +116,7 @@
                  (> (modtime proto-path) (modtime class-dest)))
          (binding [*compile-protobuf?* false]
            (.mkdirs dest)
-           (binding [*compile-java?* false]
-             (extract-dependencies project proto-path protos proto-dest))
+           (extract-dependencies project proto-path protos proto-dest)
            (doseq [proto protos]
              (let [args (into [(.getPath (protoc project)) proto
                                (str "--java_out=" (.getAbsoluteFile dest)) "-I."]
