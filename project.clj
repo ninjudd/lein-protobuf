e34f71865247ebdc42d36dee2c55ac583c89f791
Elango Cheran <elango.cheran@gmail.com>
2016-01-23 21:28:08 -0800

    version 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protobuf "0.5.0-SNAPSHOT"
+(defproject lein-protobuf "0.5.0"
   :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
